2838264c2003d5782eaea661defa81dd248049b3
cateee69alt <93328773+cateee69alt@users.noreply.github.com>
2021-10-28 14:14:36 +0100

    Fixed joiner

--- raider.py
+++ raider.py
@@ -37,8 +37,38 @@ class DiscordRaid():
 
 
 def bot_inviter(link, token):
-	apilink = "https://discordapp.com/api/v6/invite/" + link
-	headers={'Authorization': token}
+	apilink = "https://discordapp.com/api/v9/invites/" + link
+	headers = {
+            "Authorization":
+            token,
+            "accept":
+            "*/*",
+            "accept-language":
+            "en-US",
+            "connection":
+            "keep-alive",
+            "cookie":
+            f'__cfduid={os.urandom(43).hex()}; __dcfduid={os.urandom(32).hex()}; locale=en-US',
+            "DNT":
+            "1",
+            "origin":
+            "https://discord.com",
+            "sec-fetch-dest":
+            "empty",
+            "sec-fetch-mode":
+            "cors",
+            "sec-fetch-site":
+            "same-origin",
+            "referer":
+            "https://discord.com/channels/@me",
+            "TE":
+            "Trailers",
+            "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.9001 Chrome/83.0.4103.122 Electron/9.3.5 Safari/537.36",
+            "X-Super-Properties":
+            "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRGlzY29yZCBDbGllbnQiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJjbGllbnRfdmVyc2lvbiI6IjEuMC45MDAxIiwib3NfdmVyc2lvbiI6IjEwLjAuMTkwNDIiLCJvc19hcmNoIjoieDY0Iiwic3lzdGVtX2xvY2FsZSI6ImVuLVVTIiwiY2xpZW50X2J1aWxkX251bWJlciI6ODMwNDAsImNsaWVudF9ldmVudF9zb3VyY2UiOm51bGx9"
+        }
+
 	bot_invite = requests.post(apilink, headers=headers)
 	print(bot_invite.text)
 
